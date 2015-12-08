dbdb232e2ccdd505b69718d337a12620c353dcd2
haseeb736 <haseeb736@gmail.com>
2015-12-08 15:38:21 +0530

    Update index.php
    saved from

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "this is Abdul Haseeb from Sublime";
-=======
-echo "this  Changed by Abdul Haseeb";
->>>>>>> 2a1e43edfe932bf498e48b0e8570ce800899f75b
 
 echo "Added this line from git yes";
 
