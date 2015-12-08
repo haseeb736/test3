8f71d69ff926aa4f7fbc08b24a28a0780256837f
haseeb <haseeb736@gmail.com>
2015-12-08 13:40:17 +0530

    Commited from local

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-echo "this is Abdul Haseeb";
+echo "this is Abdul Haseeb from Sublime";
 
 echo "Added this line from git";
 
-echo "I am going to add this line from sublime";
+echo "I am going to add this line from sublime Ok";
 
 ?>
